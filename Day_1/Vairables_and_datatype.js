2f63209b7c67d70126927e2be9cfccdd7b780fbc
Gaikwad Atharv <146733528+GaikwadAtharv@users.noreply.github.com>
2024-07-14 16:32:57 +0000

    Day-1-task-complete

---
+++ Day_1/Vairables_and_datatype.js
@@ -0,0 +1,54 @@
+// Activity-1
+
+var num1 = 5863;
+console.log(num1);
+
+let str1 = "Hello Sage!!!";
+console.log(str1);
+console.log("\n");
+
+// Activity-2
+
+const isMale = true;
+console.log(isMale);
+console.log("\n");
+
+// Activity-3
+
+const varNum = 13;
+const varBigNum = BigInt(12345678901234567890);
+const varStr = "Hello World";
+const varBool = true;
+const varObj = { name: "Rutveek", age: "23" };
+const varArr = [10, 20, 30];
+const varUndi = undefined;
+const varNull = null;
+
+console.log(varNum + "=>" + typeof varNum);
+console.log(varBigNum + "=>" + typeof varBigNum);
+console.log(varStr + "=>" + typeof varStr);
+console.log(varBool + "=>" + typeof varBool);
+console.log(varObj + "=>" + typeof varObj);
+console.log(varArr + "=>" + typeof varArr);
+console.log(varUndi + "=>" + typeof varUndi);
+console.log(varNull + "=>" + typeof varNull);
+console.log("\n");
+
+// Activity-4
+console.log("Reassignment in let:");
+let tempStr = "Sage";
+console.log("Assigning Value:\n tempStr=", tempStr);
+
+tempStr = "Rutveek";
+console.log("Reassigning Value:\n tempStr=", tempStr);
+console.log("\n");
+
+// Activity-5
+
+console.log("Reassignment in const:");
+const tempNum = 5;
+console.log("Assigning Value:\n tempNum=", tempNum);
+
+tempNum = 7;
+console.log("Reassigning Value:\n tempNum=", tempNum);
+console.log("\n");
