9581037f5849dd90047bbec2b0a3f46e841474cb
Gaikwad Atharv <146733528+GaikwadAtharv@users.noreply.github.com>
2024-07-15 12:30:29 +0000

    Day-2-complete

---
+++ Day_2/Operator.js
@@ -0,0 +1,83 @@
+// Activity-1
+let num1 = 20;
+let num2 = 10;
+
+// Task-1
+let sum = num1 + num2;
+console.log(sum);
+
+// Task-2
+let sub = num1 - num2;
+console.log(sub);
+
+// Task-3
+let multi = num1 * num2;
+console.log(multi);
+
+// Task-4
+let div = num1 / num2;
+console.log(div);
+
+// Task-5
+let rem = num1 % num2;
+console.log(rem);
+
+// Activity-2
+console.log("\n");
+
+// Task-6
+let asSum = 10;
+asSum +=20;
+console.log(asSum);
+
+// Task-7
+let asSub = 20;
+asSub -=10;
+console.log(asSub);
+
+// Activity-3
+console.log("\n");
+
+// Task-8
+let compLess = 10 < 20;
+console.log(compLess);
+
+let compGreat = 10 > 20;
+console.log(compGreat)
+
+// Task-9
+let compLessEq = 10 <= 20;
+console.log(compLessEq)
+
+let compGreatEq = 10 >= 20;
+console.log(compGreatEq);
+
+// Task-10
+let compEq = 10 == "10";
+console.log(compEq);
+
+let compEq2 = 10 === "10";
+console.log(compEq2);
+
+// Activity-4
+console.log("\n");
+
+// Task-11
+let opAnd = true && false;
+console.log(opAnd);
+
+// Task-12
+let opOr = true || false;
+console.log(opOr);
+
+// Task-13
+let opNot = !true;
+console.log(opNot);
+
+// Activity-5
+console.log("\n");
+
+// Task-14
+let num = -1;
+let intStat = num>=0? "Positive":"Negative";
+console.log(intStat);
